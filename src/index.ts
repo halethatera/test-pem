64784683d2009b55103c255fb76ac0c0d0a8b528
halethatera <167048626+halethatera@users.noreply.github.com>
2024-12-07 12:27:41 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import path from 'path'
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.get('/.well-known/appspecific/com.tesla.3p.public-key.pem', (req, res) => {
     res.sendFile(path.join(__dirname, 'key/com.tesla.3p.public-key.pem'));
